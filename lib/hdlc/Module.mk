75b4cebd14d8eaa6c99d19a8d9a8f8260c51d922
belegar <anthony.rabine@tarotclub.fr>
2017-03-29 18:28:49 +0200

    separation between ip and hdlc transport protocols

---
+++ lib/hdlc/Module.mk
@@ -0,0 +1,5 @@
+
+LOCAL_DIR = $(call my-dir)/
+
+SOURCES += $(addprefix $(LOCAL_DIR), hdlc.c)
+
--- lib/transport/Module.mk
+++
@@ -1,5 +0,0 @@
-
-LOCAL_DIR = $(call my-dir)/
-
-SOURCES += $(addprefix $(LOCAL_DIR), hdlc.c tcp_server.c)
-
