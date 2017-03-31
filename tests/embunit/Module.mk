4bd490df967ec0e8e7fbd657dacd733173869a9c
belegar <anthony.rabine@tarotclub.fr>
2017-03-31 10:54:25 +0200

    Changed again test engine for C++, added clock test

--- tests/embunit/Module.mk
+++
@@ -1,5 +0,0 @@
-
-LOCAL_DIR = $(call my-dir)/
-
-SOURCES += $(addprefix $(LOCAL_DIR), AssertImpl.c CompilerOutputter.c RepeatedTest.c stdImpl.c TestCaller.c TestCase.c TestResult.c TestRunner.c TestSuite.c  TextOutputter.c TextUIRunner.c XMLOutputter.c)
-
