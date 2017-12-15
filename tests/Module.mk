b38220a6b15db46a83115b74b8676f399c0c0877
Anthony Rabine <anthony@rabine.fr>
2017-12-15 15:22:23 +0100

    rm tests from lib

--- tests/Module.mk
+++
@@ -1,7 +0,0 @@
-
-LOCAL_DIR = $(call my-dir)/
-
-# test_aes128gcm.c test_csm_array.c
-
-SOURCES += $(addprefix $(LOCAL_DIR), main.cpp test_clock.cpp test_association.cpp test_csm_array.cpp test_cosem_read_by_block.cpp test_hdlc.cpp cosem_hal.c)
-
